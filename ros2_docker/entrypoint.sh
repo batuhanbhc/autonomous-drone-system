4089ce7f30d520c31928c54eb8f794825d8b4824
Batuhan Bahçeci <batuhan.bahceci@sabanciuniv.edu>
2026-02-16 11:26:45 +0300

    setup container to use ros2 humble (previously ros2 jazzy)

--- ros2_docker/entrypoint.sh
+++ ros2_docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-set -e
+set +e
 
-source /opt/ros/jazzy/setup.bash
+source /opt/ros/humble/setup.bash
 
 if [ -f /home/batuhan/ros2_ws/install/setup.bash ]; then
     source /home/batuhan/ros2_ws/install/setup.bash
@@ -17,5 +17,5 @@ echo "ROS_DOMAIN_ID updated to $ROS_DOMAIN_ID"
 
 # Set hailotools environment variables
 export HAILO_TAPPAS=/opt/hailo/tappas
-export GST_PLUGIN_PATH=/opt/hailo/tappas/lib/aarch64-linux-gnu/gstreamer-1.0${GST_PLUGIN_PATH:+:$GST_PLUGIN_PATH}
+export GST_PLUGIN_PATH=/usr/lib/aarch64-linux-gnu/gstreamer-1.0:/opt/hailo/tappas/lib/aarch64-linux-gnu/gstreamer-1.0${GST_PLUGIN_PATH:+:$GST_PLUGIN_PATH}
 export LD_LIBRARY_PATH=/opt/hailo/tappas/lib/aarch64-linux-gnu:/opt/hailo/tappas/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
