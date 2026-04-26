8df4a01d82531aa6b2a265cac7944b639bf14bc0
batuhan <batuhan.bahceci@sabanciuniv.edu>
2026-04-26 17:32:52 +0300

    change gps data hz from 2 to 4

--- ros2_ws/src/mavros_gate/src/utils.cpp
+++ ros2_ws/src/mavros_gate/src/utils.cpp
@@ -313,7 +313,7 @@ bool ControlGateNode::initializationRoutine() {
   requestMsgInterval(245, 2.0f,  "EXTENDED_SYS_STATE");
   requestMsgInterval(147, 1.0f,  "BATTERY_STATUS");
   requestMsgInterval(32,  60.0f, "LOCAL_POSITION_NED");
-  requestMsgInterval(24,  2.0f,  "GPS_RAW_INT");
+  requestMsgInterval(24,  4.0f,  "GPS_RAW_INT");
   requestMsgInterval(31,  60.0f, "ATTITUDE_QUATERNION");
 
   initCommandHandlers();
